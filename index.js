e55e0e0cad0558b1352aa038466b902b77c44548
Kevin Poppe <Kevin@KevinPoppe.com>
2022-12-30 14:17:43 +0100

    added mail-validator

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const iban = require('./functions/iban/iban.js');
+
+const isValidMail = require('./functions/mail/mail.js');
+
 const isNumeric = require('./functions/characters/numeric/numeric.js');
 const isAlphabet = require('./functions/characters/alphabet/alphabet.js');
 const isAlphaNumeric = require('./functions/characters/alphanumeric/alphanumeric.js');
 const isAlphaPunctuation = require('./functions/characters/alphaPunctuation/alphaPunctuation.js');
 const isAlphaNumericPunctuation = require('./functions/characters/alphaNumericPunctuation/alphaNumericPunctuation.js');
 
-function test(val) {
-  return val === 'test';
-}
-
 module.exports = {
-  test,
   iban,
   isNumeric,
   isAlphabet,
   isAlphaNumeric,
   isAlphaPunctuation,
   isAlphaNumericPunctuation,
+  isValidMail,
 };
