df6cd82bc1d6210af4bf220a72538e607bbb01ba
Kevin Poppe <Kevin@KevinPoppe.com>
2023-01-14 22:23:01 +0100

    cleaned package

--- sort.js
+++
@@ -1,9 +0,0 @@
-var { blzList } = require('./functions/iban/blzList');
-
-const asArray = Object.entries(blzList.de); // convert object to array
-
-const filtered = asArray.map(([key, value]) => {
-  return { key: value };
-});
-
-console.log(filtered);
